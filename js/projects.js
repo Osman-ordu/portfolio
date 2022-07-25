f8fa9143ee2288ce2b46013b4c32c9ebfc7270f0
Osman Ordu <orduosmann@gmail.com>
2022-07-26 00:11:44 +0300

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -12,7 +12,7 @@ const projects = [
             {
                 id:getProjectId(),
                 isRight: true,
-                projectImg:'/images/project-photo/random-choice-picker.png',
+                projectImg:'./images/project-photo/random-choice-picker.png',
                 projectName:'Random Choice Picker',
                 description:'Girilen ifadeler virgüllerle birbirinden ayrılarak birbirinden farklı metin veya sayılardan oluşabilir. Olay dinleyicisi sayesinde "Enter" tuşuna tıklanıldığında girilen değerler arasında rastgele seçim yapan bir döngü oluşturur.',
                 projectUrl:'https://github.com/Osman-ordu/Random-choice-picker',
@@ -26,7 +26,7 @@ const projects = [
             {
                 id:getProjectId(),
                 isRight: true,
-                projectImg:'/images/project-photo/weather-app.png',
+                projectImg:'./images/project-photo/weather-app.png',
                 projectName:'Weather App',
                 description:'Dünyadaki tüm şehirlerin hava durumu derecelendirmelerini ve hava durumu açıklamasını gösterir.',
                 projectUrl:'https://github.com/Osman-ordu/Global-weather-app',
@@ -40,7 +40,7 @@ const projects = [
             {
                 id:getProjectId(),
                 isRight: true,
-                projectImg:'/images/project-photo/pick-game.png',
+                projectImg:'./images/project-photo/pick-game.png',
                 projectName:'Pick Game',
                 description:'Bir zar atma oyunudur,iki kullanıcı vardır ,kullanıcılar zarı attığında 1 gelirse skor sıfırlanır ve zar atma sırası rakip kullanıcıya geçer, skoru 50 olan kullanıcı kazanır. ',
                 projectUrl:'https://github.com/Osman-ordu/Pick-game',
@@ -54,7 +54,7 @@ const projects = [
             {
                 id:getProjectId(),
                 isRight: true,
-                projectImg:'/images/project-photo/go-screen.png',
+                projectImg:'./images/project-photo/go-screen.png',
                 projectName:'Go Screen',
                 description:'Kullanıcı videoyu başlattığında, video planlanan dakika aralığında durdurulur. Cevap butonları görüntülenir. Seçilen buton değeri 1 artar. Video bittiğinde puan sonuçları görüntülenir.',
                 projectUrl:'https://github.com/Osman-ordu/Go-screen',
@@ -68,7 +68,7 @@ const projects = [
             {
                 id:getProjectId(),
                 isRight: true,
-                projectImg:'/images/project-photo/shopping-cart.png',
+                projectImg:'./images/project-photo/shopping-cart.png',
                 projectName:'Shopping Cart',
                 description:'Bir sepete ekleme uygulamasıdır.Kartların olduğu arayüzde satın alınacak ürünü sepete eklediğimizde Navbar kısmında "Added to card" yazısı animasyonlu bir şekilde karşımıza çıkar.Sepet değeri her kart eklendiğinde eklenilen ürün kadar artar ve silinildiğinde sepetin uzunluğu kadar silinir. ',
                 projectUrl:'https://github.com/Osman-ordu/Shopping-cart',
@@ -82,7 +82,7 @@ const projects = [
             {
                 id:getProjectId(),
                 isRight: true,
-                projectImg:'/images/project-photo/to-do-list.png',
+                projectImg:'./images/project-photo/to-do-list.png',
                 projectName:'To Do List',
                 description:'Yapılacaklar listesi',
                 projectUrl:'https://github.com/Osman-ordu/To-do-list',
@@ -96,7 +96,7 @@ const projects = [
             {
                 id:getProjectId(),
                 isRight: true,
-                projectImg:'/images/project-photo/netflix-clone.png',
+                projectImg:'./images/project-photo/netflix-clone.png',
                 projectName:'Netflix Clone',
                 description:'Netflix Arayüzü',
                 projectUrl:'https://github.com/Osman-ordu/Netflix-clone',
@@ -110,7 +110,7 @@ const projects = [
             {
                 id:getProjectId(),
                 isRight: true,
-                projectImg:'/images/project-photo/key-code.png',
+                projectImg:'./images/project-photo/key-code.png',
                 projectName:'Key code',
                 description:'Tüm klavye tuşlarını bir özelliğinin olduğunu gösteren arayüz.',
                 projectUrl:'https://github.com/Osman-ordu/Keycode',
